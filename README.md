96ef00883c5ed62a6906e89e2fe1152f648b6f57
Madhav Khandhar <madhavkhandhar@gmail.com>
2021-11-02 04:28:30 +0000

    Created baseline for header template

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Warisan Nexus website 
 
-Start the development server with `python -m flask run`
+Start the development server with `python app.py`
--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return"<p>Hello world</p>"
+    return render_template('pages/index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
